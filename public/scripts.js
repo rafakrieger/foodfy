3e5db4c680867f0a285af4b98d7b6b1037104957
rafakrieger <rafabruno@gmail.com>
2020-04-01 03:09:20 -0300

    refactoring concluded

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,22 @@
 const recipes = document.querySelectorAll(".card")
+const button = document.querySelectorAll(".button")
+const description = document.querySelectorAll(".recipe-description")
 
 for (let recipe of recipes) {
     recipe.addEventListener("click", function(){
         const recipeId = recipe.getAttribute("id")
         window.location.href = `/recipes/${recipeId}`
+    })
+}
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", function(){       
+        if (description[i].classList.contains("hide")) {
+            button[i].textContent = "ESCONDER"
+            description[i].classList.remove("hide")
+        }else {
+            button[i].textContent = "MOSTRAR"
+            description[i].classList.add("hide")
+        }
     })
 }
